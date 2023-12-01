03ace2c5bcafc373cdbbfc1c63266a359c1fc7c9
prarthanagalani <prarthanagalani@gmail.com>
2023-12-01 18:48:50 +0530

    added: Uc1: create stack using LL-day9

--- StackMain.java
+++ StackMain.java
@@ -1,9 +1,53 @@
 package StackUsingLinkedList;
 
-public class StackMain{
+import java.util.LinkedList;
 
-    public static void main(String args[])
-    {
-        
+
+public class StackMain {
+
+    public static void main(String args[]) {
+        Stack<Integer> stack = new Stack<>();
+
+        // Push elements onto the stack
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+
+        // Display the initial stack
+        System.out.println("Initial Stack:");
+        stack.display();
+
+    }
+}
+
+class Stack<T> {
+    LinkedList<T> List = new LinkedList<>();
+
+    // push
+    public void push(T data) {
+        List.addFirst(data);
+    }
+
+    // pop
+    public T pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return null;
+        }
+        return List.removeFirst();
+
+    }
+
+    // Check if the stack is empty
+    public boolean isEmpty() {
+        return List.isEmpty();
+    }
+
+    // Display the stack
+    public void display() {
+        for (T data : List) {
+            System.out.print(data + " ");
+        }
+
     }
 }
